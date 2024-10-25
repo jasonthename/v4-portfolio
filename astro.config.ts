c90ee468e8b03559edd56a8b6be59d42ea06bc8b
Jason J <jjackson8895@gmail.com>
2024-10-25 18:03:47 -0400

    Changing stuff v3

--- astro.config.ts
+++ astro.config.ts
@@ -6,7 +6,7 @@ import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 import tailwind from '@astrojs/tailwind';
 import mdx from '@astrojs/mdx';
-import netlify from '@astrojs/netlify';
+import netlify from "@astrojs/netlify/functions";
 import partytown from '@astrojs/partytown';
 import icon from 'astro-icon';
 import compress from 'astro-compress';
